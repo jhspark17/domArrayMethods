3959f2f1b0ee24fd3e8fa4f741cf57bb7a36fac4
Joseph Park <jhspark17@gmail.com>
2020-07-01 11:20:36 -0700

    Add addData function

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const buttons = document.getElementById('buttons')
+const info = document.getElementById('info');
+
+for (let i = 0; i < 3; i++) addUser();
 
 function applyAction(action) {
   if (action === "add") addUser();
 }
 
 async function addUser() {
-  let res = await fetch("https://randomuser.me/api")
-  let data = await res.json();
-  console.log(data)
+  const res = await fetch("https://randomuser.me/api")
+  const data = await res.json();
+  const first = data.results[0].name.first
+  const last = data.results[0].name.last
+  const user = {name: `${first} ${last}`, wealth: `$${(Math.random() * 100000000).toFixed(2)}`}
+  addData(user);
+}
+
+function addData(user) {
+  const person = document.createElement('div');
+  person.innerHTML = `<strong>${user.name}</strong> ${user.wealth}`
+  info.appendChild(person);
 }
 
 
