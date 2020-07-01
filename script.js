4cbaa04bb687f575cf23f20fc2e366c58abefe7d
Joseph Park <jhspark17@gmail.com>
2020-06-30 23:29:17 -0700

    Initialize addUserfunction

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const buttons = document.getElementById('buttons')
 
+function applyAction(action) {
+  if (action === "add") addUser();
+}
 
+async function addUser() {
+  let res = await fetch("https://randomuser.me/api")
+  let data = await res.json();
+  console.log(data)
+}
 
 
 
 
 buttons.addEventListener('click', e => {
-  console.log(e.target.id)
+  applyAction(e.target.id)
 })
 
 
