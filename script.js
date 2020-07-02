b1cf63b344325ed502fbbb2f644bd8e3c7a9bed1
Joseph Park <jhspark17@gmail.com>
2020-07-01 21:46:44 -0700

    Change CSS

--- script.js
+++ script.js
@@ -7,10 +7,21 @@ let people = [];
 
 function applyAction(action) {
   if (action === "add") addUser();
-  if (action === "calc") calculateWealth();
-  if (action === "sort") sortPeople();
   if (action === "show") onlyMills();
+  if (action === "sort") sortPeople();
   if (action === "doub") doubleMoney();
+  if (action === "calc") calculateWealth();
+}
+
+async function addUser() {
+  total.innerHTML = "";
+  const res = await fetch("https://randomuser.me/api")
+  const data = await res.json();
+  const last = data.results[0].name.last
+  const first = data.results[0].name.first
+  const user = {name: `${first} ${last}`, wealth: Math.floor(Math.random() * 10000000)}
+  people.push(user);
+  updateDom();
 }
 
 function doubleMoney(){
@@ -27,23 +38,13 @@ function sortPeople() {
   updateDom(people.sort((a, b) => b.wealth - a.wealth));
 }
 
-async function addUser() {
-  total.innerHTML = "";
-  const res = await fetch("https://randomuser.me/api")
-  const data = await res.json();
-  const first = data.results[0].name.first
-  const last = data.results[0].name.last
-  const user = {name: `${first} ${last}`, wealth: Math.floor(Math.random() * 10000000)}
-  people.push(user);
-  updateDom();
-}
 
 function calculateWealth(){
   let currTotal = 0;
-  people.forEach((person) => currTotal += person.wealth)
+  let wealth = people.reduce((acc, p) => (acc += p.wealth), 0);
   let curr = document.createElement('div');
   curr.classList.add('curr-total')
-  curr.innerHTML = `<strong>Total</strong> $ ${currTotal}`;
+  curr.innerHTML = `<strong>Total</strong> $ ${wealth}`;
   total.appendChild(curr);
 }
 
@@ -60,9 +61,5 @@ function updateDom(){
 }
 
 
-buttons.addEventListener('click', e => {
-  applyAction(e.target.id)
-})
-
-total.addEventListener('click', calculateWealth)
-
+buttons.addEventListener('click', e => applyAction(e.target.id))
+// total.addEventListener('click', calculateWealth)
